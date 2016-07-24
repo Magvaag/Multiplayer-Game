d95b7d169e007bce8c8d5648aa03c097730b7faf
ScratchForFun <magnusvaagenmorud@gmail.com>
2016-07-24 14:30:06 +0200

    Fixed duplicates

--- src/net/vaagen/game/multiplayer/ClientPackageListener.java
+++ src/net/vaagen/game/multiplayer/ClientPackageListener.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/net/vaagen/game/multiplayer/ClientPackageListener.java
 package net.vaagen.game.multiplayer;
 
 import java.io.BufferedReader;
@@ -35,41 +34,3 @@ public class ClientPackageListener extends Thread {
     }
 
 }
-=======
-package net.vaagen.game.multiplayer;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.Socket;
-
-/**
- * Created by Magnus on 2/10/2016.
- */
-public class ClientPackageListener extends Thread {
-
-    private Client client;
-    private Socket socket;
-
-    public ClientPackageListener(Client client, Socket socket) {
-        this.client = client;
-        this.socket = socket;
-
-        start();
-    }
-
-    public void run() {
-        while (!socket.isClosed() && socket.isConnected()) {
-            try {
-                BufferedReader inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                String sPackage = inputStream.readLine();
-
-                client.readPackage(sPackage);
-            } catch (IOException e) {
-                client.disconnectFromServer();
-            }
-        }
-    }
-
-}
->>>>>>> origin/master:src/net/vaagen/game/src/net/vaagen/game/multiplayer/ClientPackageListener.java
